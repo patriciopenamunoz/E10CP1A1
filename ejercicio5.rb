554bb0532fb20679f05fc704c98b8304edd89efa
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-08-05 13:00:57 -0400

    ADD ejercicio5.rb

---
+++ ejercicio5.rb
@@ -0,0 +1,35 @@
+# Transformar la clase Herviboro en un módulo. Implementar el módulo en
+# la clase Conejo mediante Mixin para poder acceder al método dieta desde la
+# instancia. Finalmente imprimir la definición de Hervíboro.
+
+class Herviboro
+  @@definir = 'Sólo me alimento de vegetales!'
+
+  def self.definir
+    @@definir
+  end
+
+  def dieta
+    "Soy un Herviboro!"
+  end
+end
+
+class Animal
+  def saludar
+    "Soy un animal!"
+  end
+end
+
+class Conejo < Animal < Herviboro
+  def initialize(name)
+    @name = name
+  end
+end
+
+conejo = Conejo.new('Bugs Bunny')
+conejo.saludar
+conejo.dieta
+Herviboro.definir
+
+# Pregunta: ¿Por qué es mejor solución la implementación de Mixin que
+#            mediante Herencia de Herencia?
